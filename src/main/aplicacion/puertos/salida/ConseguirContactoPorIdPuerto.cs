18e7d603788bab91a818a3c4a06e4f5b8058d1ca
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-09 22:25:23 -0500

    Cambio de nombre archivo

--- src/main/aplicacion/puertos/salida/ConseguirContactoPorIdPuerto.cs
+++ src/main/aplicacion/puertos/salida/ConseguirContactoPorIdPuerto.cs
@@ -2,7 +2,7 @@ using Dominio.OV;
 
 namespace Aplicacion.Puertos.Salida;
 
-public interface ConseguirContactoPorIdPuerto
+public interface IConseguirContactoPorIdPuerto
 {
     byte? GetIndicePorId(Id id);
 }
