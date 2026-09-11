1ed5568814f0a6061247f267340bfccc9236ad65
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-10 20:24:32 -0500

    Archivos del sistema y ultimos cambios

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Infraestructura.Configuraciones;
+
+namespace Main;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        ContainerDependencia container =
+            new ContainerDependencia();
+
+        container.AgendaTelefonicaCli().Start();
+    }
+}
+
